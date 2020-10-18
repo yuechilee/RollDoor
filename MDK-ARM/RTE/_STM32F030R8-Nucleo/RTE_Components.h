4c7cfe89e8aaa38a84e508dad15cebb6ecb5fb5f
ycl176 <ychi.lee@gmail.com>
2020-10-18 13:26:23 +0800

    Begin version based on Nucleo-STM32f030's GPIO Toggle example.

---
+++ MDK-ARM/RTE/_STM32F030R8-Nucleo/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Project' 
+ * Target:  'STM32F030R8-Nucleo' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
